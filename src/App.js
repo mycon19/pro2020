15e491d53f80f682be78a14413fbfc5a6f2178d1
Nidhi <nidhi.raviprasad@gmail.com>
2021-03-23 19:48:04 -0700

    changes for login

--- src/App.js
+++ src/App.js
@@ -1,148 +1,58 @@
-import React, { Component } from 'react';
-import update from 'react-addons-update';
-import quizQuestions from './api/quizQuestions';
-import Quiz from './components/Quiz';
-import Result from './components/Result';
-import got from './images/got.jpg';
-import logo from './svg/logo.svg';
-import './App.css';
- 
-class App extends Component {
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
 
-  constructor(props) {
-    super(props);
- 
-    this.state = {
-      counter: 0,
-      questionId: 1,
-      question: '',
-      answerOptions: [],
-      answer: '',
-      answersCount: {
-        Ravenclaw: 0,
-        Hufflepuff: 0,
-        Slytherin: 0,
-	    Gryffindor: 0
-      },
-      result: ''
-    };
- 
-    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
-  }
- 
-  componentWillMount() {
-    const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));
-    this.setState({
-      question: quizQuestions[0].question,
-      answerOptions: shuffledAnswerOptions[0]
-    });
-  }
- 
-  shuffleArray(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
- 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
- 
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
- 
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+import PrivateRoute from './Utils/PrivateRoute';
+import PublicRoute from './Utils/PublicRoute'; 
+import {getToken, removeUserSession, setUserSession} from './Utils/Common';
+
+import Login from './Login';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import Axios from 'axios';
+
+function App() {
+
+  const[authLoading, setAuthLoading] = useState(true);
+
+  useEffect (() => {
+    const token = getToken();
+    if(!token) {
+      return;
     }
- 
-    return array;
-  };
- 
-  handleAnswerSelected(event) {
-    this.setUserAnswer(event.currentTarget.value);
- 
-    if (this.state.questionId < quizQuestions.length) {
-        setTimeout(() => this.setNextQuestion(), 300);
-    } else {
-        setTimeout(() => this.setResults(this.getResults()), 300);
-    }
-  }
- 
-  setUserAnswer(answer) {
-    const updatedAnswersCount = update(this.state.answersCount, {
-      [answer]: {$apply: (currentValue) => currentValue + 1}
+
+    Axios.get("http://localhost:3000/verifyToken?token=$ABCDEF$123").then(response => {
+      setUserSession(response.data.token, response.data.user);
+      setAuthLoading(false);
+    }).catch(error => {
+      removeUserSession();
+      setAuthLoading(false);
     });
- 
-    this.setState({
-        answersCount: updatedAnswersCount,
-        answer: answer
-    });
+  }, []);
+
+  if (authLoading && getToken()) {
+    return <div className="content"> Checking Authentication...</div>
   }
- 
-  setNextQuestion() {
-    const counter = this.state.counter + 1;
-    const questionId = this.state.questionId + 1;
- 
-    this.setState({
-        counter: counter,
-        questionId: questionId,
-        question: quizQuestions[counter].question,
-        answerOptions: quizQuestions[counter].answers,
-        answer: ''
-    });
-  }
- 
-  getResults() {
-    const answersCount = this.state.answersCount;
-    const answersCountKeys = Object.keys(answersCount);
-    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
-    const maxAnswerCount = Math.max.apply(null, answersCountValues);
- 
-    return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount);
-  }
- 
-  setResults(result) {
-    if (result.length === 1) {
-      this.setState({ result: result[0] });
-    } else {
-      this.setState({ result: 'Hatstall' });
-    }
-  }
- 
-  renderQuiz() {
-    return (
-      <Quiz
-        answer={this.state.answer}
-        answerOptions={this.state.answerOptions}
-        questionId={this.state.questionId}
-        question={this.state.question}
-        questionTotal={quizQuestions.length}
-        onAnswerSelected={this.handleAnswerSelected}
-      />
-    );
-  }
- 
-  renderResult() {
-    return (
-      <Result quizResult={this.state.result} />
-    );
-  }
- 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" /> 
-          <img src={got} className="App-img" alt="img" />
-          
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/login">Login</NavLink>
+            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/dashboard" component={Dashboard}/>
+            </Switch>
+          </div>
         </div>
-        {this.state.result ? this.renderResult() : this.renderQuiz()}
-      </div>
-    );
- 
- 
- 
- 
-  }
- 
+      </BrowserRouter>
+    </div>
+  );
 }
+
 export default App;
